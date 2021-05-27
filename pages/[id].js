35b546aa1056757bda46c046e07184771dc2809d
huseyinglc <huseyingulcuu@gmail.com>
2021-05-27 17:04:29 +0300

    to make pre-rendered page with fetched data, I added the getstaticprops function

---
+++ pages/[id].js
@@ -0,0 +1,7 @@
+import MeetupDetail from "../components/meetups/MeetupDetail";
+
+const PageDetail = () => {
+  return <MeetupDetail image="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1200px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg" title="First Meetup" address="Some Street 5" description="This is a first meetup" />;
+};
+
+export default PageDetail;
