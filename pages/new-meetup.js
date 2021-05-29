c8055d8630e303d505f8e8ae206c5bdb4e6b8cf1
huseyinglc <huseyingulcuu@gmail.com>
2021-05-29 15:58:17 +0300

    connected with mongodb cluster and sent a post request to internal api

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,13 +1,23 @@
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
-
-
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 const NewMeetupPage = () => {
+  const router = useRouter();
 
-    const onAddMeetupHandler = (enteredMeetupData) => {
-        console.log(enteredMeetupData);
-    }
+  const onAddMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
 
-    return <NewMeetupForm onAddMeetup={onAddMeetupHandler} /> 
-}
+    router.push("/");
+  };
+
+  return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />;
+};
 
 export default NewMeetupPage;
