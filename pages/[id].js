bac98a58ec46fec7bc340e81b0fdab4f8c39504f
huseyinglc <huseyingulcuu@gmail.com>
2021-05-29 17:33:31 +0300

    dummy data where i used to hard coded without fetched db is replaced with the data getting from mongodb

--- pages/[id].js
+++ pages/[id].js
@@ -1,30 +1,38 @@
 import MeetupDetail from "../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
-const PageDetail = () => {
+const PageDetail = (props) => {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1200px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg"
-      title="First Meetup"
-      address="Some Street 5"
-      description="This is a first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
 export const getStaticPaths = async () => {
+  MongoClient.connect();
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin:admin@cluster0.vygtp.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
+
   return {
-    paths: [
-      {
-        params: {
-          id: "m1",
-        },
+    paths: meetups.map((meetup) => ({
+      params: {
+        id: meetup._id.toString(),
       },
-      {
-        params: {
-          id: "m2",
-        },
-      },
-    ],
+    })),
+
     fallback: false,
   };
 };
@@ -32,17 +40,29 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
   const meetupId = context.params.id;
 
-  console.log(meetupId);
+  MongoClient.connect();
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin:admin@cluster0.vygtp.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+  client.close();
 
   return {
     props: {
       meetupData: {
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1200px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg",
-        id: meetupId,
-        title: "First Meetup",
-        address: "Some Street 5",
-        description: "This is a first meetup",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
